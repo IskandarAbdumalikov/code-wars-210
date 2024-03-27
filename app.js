2a224d7ac37bad145097095e8eebbb4faac8cb82
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-03-27 18:07:54 +0500

    added

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-function isItLetter(character) {
-  return isNaN(character);
-}
+// {function isItLetter(character) {
+//   return isNaN(character);
+// }
 
-console.log(isItLetter(3));
+// console.log(isItLetter(3));
+// }
+// {
+//    function removeNumbers(text) {
+//      let result = "";
+//      for (let i = 0; i < s.length; i++) {
+//        const char = s[i];
+//        if (isNaN(parseInt(char))) {
+//          result += char;
+//        }
+//      }
+//      return result;
+//    }
+//    console.log(removeNumbers("sa3l4o9m"));
+// }
+// {
+//     function removeNumbers(text) {
+//       let result = "";
+//       for (let i = 0; i < text.length; i++) {
+//         if (isNaN(text[i])) {
+//             continue
+//         }
+         
+//       }
+//       return result;
+//     }
+// }
